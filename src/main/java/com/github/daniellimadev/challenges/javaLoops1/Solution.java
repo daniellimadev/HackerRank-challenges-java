cc7258c9bc0517c7bd2e99e9ab3d3aa51230ca77
daniellimadev <pdaniel643@gmail.com>
2024-06-20 13:04:49 -0300

    Challenge Java Loops I!

---
+++ src/main/java/com/github/daniellimadev/challenges/javaLoops1/Solution.java
@@ -0,0 +1,20 @@
+package com.github.daniellimadev.challenges.javaLoops1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        for(int i = 1; i <= 10; i++) {
+            int result = N * i;
+            System.out.println(N + " x " + i +" = " + result);
+        }
+    }
+}
